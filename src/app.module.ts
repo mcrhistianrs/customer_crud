ba3ba33317f91d958e6b888580dd55479d404072
Maicon Crhistian <maiconcrhistian@Maicons-MacBook-Pro.local>
2023-05-06 17:39:22 -0300

    it was removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './interfaces/controllers/app.controller';
-import { AppService } from './application/services/app.service';
 import { CustomerModule } from './infra/modules/customer/customer.module';
 
 @Module({
   imports: [CustomerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
